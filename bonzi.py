be2a1521d7f8addf292cadd57a90176feff7fa7d
rybawdz <95350672+rybawdz@users.noreply.github.com>
2021-12-29 19:40:22 +0100

    Update main code: animation adjustments
    Now displayed images are animations. Also, adjusted times of changing sprites, for more natural behaviours. Code readability adjustments required.

--- bonzi.py
+++ bonzi.py
@@ -4,9 +4,16 @@ import random
 import webbrowser as wb
 
 root = tk.Tk()
-label = tk.Label(root)
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+
+
+root.config(highlightbackground='black')
+label = tk.Label(root, bd=0,bg='black')
 label.pack()
+
 m = tk.Menu(root, tearoff=0)
+
 
 #opcje w menu
 def menu_nuta():
@@ -28,14 +35,107 @@ def do_popup(event):
         m.grab_release()
 label.bind('<Button-3>', do_popup)
 
-img_names = ["idle.png", "move_left.png", "move_right.png", "sleeping.png", "idle_to_sleeping.png", "sleeping_to_idle.png"]
-images = [tk.PhotoImage(file=img_names[i]) for i in range(len(img_names))]
-def image_choice(event_num): #dobiera sprite'a do eventu
-    img = images[event_num]
-    prev_event = event_num
+
+
+#arrays of names of frames
+event_names = ["im_idle", "im_left" ,"im_right" , "im_sleeping", "im_idle_sleeping", "im_sleeping_idle"]
+
+
+idle_names = ["idle"+ str(i) + ".png" for i in range(1,5)]
+left_names = ["move_left"+ str(i) + ".png" for i in range(1,5)]
+right_names = ["move_right"+ str(i) + ".png" for i in range(1,5)]
+sleeping_names = ["sleeping"+ str(i) + ".png" for i in range(1,5)]
+idle_sleeping_names = ["idle_to_sleeping"+ str(i) + ".png" for i in range(1,5)]
+sleeping_idle_names = ["sleeping_to_idle"+ str(i) + ".png" for i in range(1,5)]
+
+im_idle = [tk.PhotoImage(file=idle_names[i]) for i in range(len(idle_names))]
+im_left = [tk.PhotoImage(file=left_names[i]) for i in range(len(left_names))]
+im_right = [tk.PhotoImage(file=right_names[i]) for i in range(len(right_names))]
+im_sleeping = [tk.PhotoImage(file=sleeping_names[i]) for i in range(len(sleeping_names))]
+im_idle_sleeping = [tk.PhotoImage(file=idle_sleeping_names[i]) for i in range(len(idle_sleeping_names))]
+im_sleeping_idle = [tk.PhotoImage(file=sleeping_idle_names[i]) for i in range(len(sleeping_idle_names))]
+
+
+
+
+def f_idle(it):
+    img = im_idle[it]
     root.geometry("100x100+500+300")
     label.config(image=img)
-    root.after(1000, event_choice, prev_event)
+    if it+1 < 4:
+        it+=1
+        root.after(100,f_idle,it)
+
+def f_left(it):
+    img = im_left[it]
+    root.geometry("100x100+500+300")
+    label.config(image=img)
+    if it+1 < 4:
+        it+=1
+        root.after(100,f_left,it)
+  
+
+def f_right(it):
+    img = im_right[it]
+    root.geometry("100x100+500+300")
+    label.config(image=img)
+    if it+1 < 4:
+        it+=1
+        root.after(100,f_right,it)
+    
+def f_sleeping(it):
+    img = im_sleeping[it]
+    root.geometry("100x100+500+300")
+    label.config(image=img)
+    if it+1 < 4:
+        it+=1
+        root.after(100,f_sleeping,it)
+
+def f_idle_sleeping(it):
+    img = im_idle_sleeping[it]
+    root.geometry("100x100+500+300")
+    label.config(image=img)
+    if it+1 < 4:
+        it+=1
+        root.after(100,f_idle_sleeping,it)
+
+def f_sleeping_idle(it):
+    img = im_sleeping_idle[it]
+    root.geometry("100x100+500+300")
+    label.config(image=img)
+    if it+1 < 4:
+        it+=1
+        root.after(100,f_sleeping_idle,it)
+
+def action(event_num):
+    prev_event = event_num
+
+    if event_num == 0:
+        f_idle(0)
+        root.after(100000000, event_choice, prev_event)
+
+    if event_num == 1:
+        f_left(0)
+        root.after(1000, event_choice, prev_event)
+
+    if event_num == 2:
+        f_right(0)
+        root.after(1000, event_choice, prev_event)
+
+    if event_num == 3:
+        f_sleeping
+        root.after(100000000, event_choice, prev_event)
+
+    if event_num == 4:
+        f_idle_sleeping
+        root.after(1000, event_choice, prev_event)
+        
+
+    if event_num == 5:
+        f_sleeping_idle
+        root.after(1000, event_choice, prev_event)
+
+
 
 def event_choice(prev_event): #wybiera kolejny event
     #idle, move_left, move_right, sleeping, idle_to_sleeping, sleeping_to_idle
@@ -48,7 +148,9 @@ def event_choice(prev_event): #wybiera kolejny event
         event_num = [0]
     else:
         event_num = random.choices(events, weights=[0.0, 0.0, 0.0, 0.8, 0.0, 0.2])
-    image_choice(event_num[0])
+
+    action(event_num[0])        
+    
 
 root.after(1, event_choice, 0)
 tk.mainloop()
